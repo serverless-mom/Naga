8f156dde94f4dc7044f4105e37a12ba8d08962ff
Toby Fee <tobyfee@gmail.com>
2015-09-14 16:00:17 -0700

    remove config file

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key: '5TPBcByMWyyEjORB6of9X3O5U'
-  , consumer_secret: 'wfE5YR1KlBKbszEEhSbAig9w5hvLLjjWdeED2JbaDY6pK0mgEI'
-  , access_token: '62817785-C56Y2VqkWCLjHzn5S72uCuRpt9Rv1h2tvWmv93iDS'
-  , access_token_secret: 'dJ1AhrICKHjkhvmUaVmTqd0M2ucTdtMqLpffOTjR6ucSX'
-}

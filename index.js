b53f9eddab8ef0036a6f91e2c8d37856ed61048d
Toby Fee <TobyFee@gmail.com>
2017-11-04 17:01:46 -0700

    My smart way of detecting whether we were running on heroku... wasn't working

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 var Twit = require('twit')
 var fs = require("fs")
-if (process.env.ACCESS_TOKEN) {
-  auth = {
+var auth = {
     consumer_key: process.env.CONSUMER_KEY,
     consumer_secret: process.env.CONSUMER_SECRET,
     access_token: process.env.ACCESS_TOKEN,
     access_token_secret: process.env.ACCESS_TOKEN_SECRET
   }
+  // var auth = require('./config.js')
 
-} else {
-  var auth = require('./config.js')
-}
 
 var twit = new Twit(auth)
 
